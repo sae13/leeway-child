d8685675159947bbc1c66a186b430da7956bb8ac
saeb <saeb@D620>
2017-04-27 20:25:09 +0430

    font change,color to purple, some translation

--- functions.php
+++ functions.php
@@ -5,3 +5,22 @@ function my_theme_enqueue_styles() {
 
 }
 ?>
+
+<?php
+// changing translations
+#http://ronangelo.com/change-or-translate-text-on-a-wordpress-theme/
+function ra_change_translate_text_multiple( $translated ) {
+	$text = array(
+		'فاقد اعتبار!' => 'نام کاربری یا رمز عبور اشتباه وارده شده است',
+		'Sorry, but nothing matched your search terms. Please try again with some different keywords.' => 'متاسفم، چیزی مشابه جست و جوی شما پیدا نشد. لطفا  کلمات دیگری را برای جست و جو امتحان کنید',
+		'Old Text 3' => 'New Translation 3',
+		'Search …' => 'جست و جو',
+		'Leave a comment' => 'ارسال نظر',
+		'Category' => 'دسته بندی',
+		'Read more' => 'متن کامل',
+	);
+	$translated = str_ireplace(  array_keys($text),  $text,  $translated );
+	return $translated;
+}
+add_filter( 'gettext', 'ra_change_translate_text_multiple', 20 );
+?>
